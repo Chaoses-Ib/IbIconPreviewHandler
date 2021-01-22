8c0bd40068d7c850e27fab3cf090c02384bdf121
Chaoses Ib <Chaos-es@outlook.com>
2021-01-22 21:24:56 +0800

    Add IconPreviewHandler

---
+++ IconPreviewHandler/IconHandlerPreviewControl.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpShell.SharpPreviewHandler;
+using System.Drawing.IconLib;
+
+namespace IconPreviewHandler
+{
+    public partial class IconHandlerPreviewControl : PreviewHandlerControl
+    {
+        public IconHandlerPreviewControl()
+        {
+            InitializeComponent();
+        }
+
+        private void AddIconsToControl()
+        {
+            BackColor = Color.White;
+
+            DoubleBuffered = true;
+
+            int yPos = 12;
+            foreach (var iconImage in iconImages)
+            {
+                var descriptionLabel = new Label
+                {
+                    Location = new Point(12, yPos),
+                    Text = $"{iconImage.Size.Width}x{iconImage.Size.Height} - {iconImage.PixelFormat}",
+                    AutoSize = true,
+                    BackColor = Color.White
+                };
+                yPos += 20;
+
+                var pictureBox = new PictureBox
+                {
+                    Location = new Point(12, yPos),
+                    Image = iconImage.Icon.ToBitmap(),
+                    Width = iconImage.Size.Width,
+                    Height = iconImage.Size.Height
+                };
+                yPos += iconImage.Size.Height + 20;
+                panelImages.Controls.Add(descriptionLabel);
+                panelImages.Controls.Add(pictureBox);
+
+                generatedLabels.Add(descriptionLabel);
+            }
+        }
+
+        public void DoPreview(string selectedFilePath)
+        {
+            try
+            {
+                var multiIcon = new MultiIcon();
+                multiIcon.Load(selectedFilePath);
+
+                foreach (var iconImage in multiIcon.SelectMany(singleIcon => singleIcon))
+                    iconImages.Add(iconImage);
+
+                AddIconsToControl();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private readonly List<Label> generatedLabels = new List<Label>();
+
+        private readonly List<IconImage> iconImages = new List<IconImage>();
+    }
+}
---
+++ IconPreviewHandler/IconPreviewHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpShell.Attributes;
+using SharpShell.SharpPreviewHandler;
+using System.Runtime.InteropServices;
+
+namespace IconPreviewHandler
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.ClassOfExtension, ".ico")]
+    [DisplayName("Ib Icon Preview Handler")]
+    [Guid("5FEB09C1-B322-4926-A2FA-614997858578")]
+    [PreviewHandler(DisableLowILProcessIsolation = false)]
+    public class IconPreviewHandler : SharpPreviewHandler
+    {
+        protected override PreviewHandlerControl DoPreview()
+        {
+            var handler = new IconHandlerPreviewControl();
+
+            if (!string.IsNullOrEmpty(SelectedFilePath))
+                handler.DoPreview(SelectedFilePath);
+
+            return handler;
+        }
+    }
+}
